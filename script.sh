1f1fef91e2c4a10ade8ef8877082a1f4bbcb099b
ananth1021 <39404224+ananth1021@users.noreply.github.com>
2018-05-24 18:13:20 +0530

    final commit
    final commit

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-/usr/local/bin/aws cloudformation create-stack --stack-name test-vpc-creation --environment-name qa --template-body file:///var/lib/jenkins/workspace/AWS_CF1/VPC_CF.txt --region ap-south-1
+/usr/local/bin/aws cloudformation create-stack --stack-name test-vpc-creation   create-environment --environment-name qa --template-body file:///var/lib/jenkins/workspace/AWS_CF1/VPC_CF.txt --region ap-south-1
